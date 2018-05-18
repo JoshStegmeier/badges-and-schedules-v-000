a4034f257185eaa7584f764e2fb3fcad6d5df8cc
Josh Stegmeier <josh@steg.me>
2018-05-18 05:14:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,12 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  newArray = []
+  array.each do |person|
+    message = badge_maker(person)
+    newArray >> message
+  end
+  return newArray
+end
